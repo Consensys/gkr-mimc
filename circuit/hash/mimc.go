0209e7a0fbdb97b5571a51726f8d79a4c1087d0e
alexandre.belling <alexandre.belling@consensys.net>
2020-12-07 19:30:01 +0100

    Updates Mimc implementation

--- circuit/hash/mimc.go
+++ circuit/hash/mimc.go
@@ -2,6 +2,7 @@ package hash
 
 import (
 	"gkr-mimc/hash"
+	"math/big"
 
 	"github.com/consensys/gnark/frontend"
 )
@@ -9,14 +10,20 @@ import (
 // MimcHash returns the result of the hashing function
 func MimcHash(cs *frontend.ConstraintSystem, stream ...frontend.Variable) frontend.Variable {
 	state := cs.Constant(0)
+	one := cs.Constant(1)
+	var keys big.Int
 	for _, m := range stream {
 		oldState := state
 		for i := 0; i < hash.MimcRounds; i++ {
-			keys := cs.Constant(hash.Arks[i])
-			state = cs.Add(keys, m, state)
+			hash.Arks[i].ToBigInt(&keys)
+			// state = cs.Add(keys, m, state)
+			tmp := cs.LinearExpression(
+				cs.Term(one, &keys),
+				cs.Term(m, big.NewInt(1)),
+				cs.Term(state, big.NewInt(1)),
+			)
 			// Applies the SBox
-			tmp := state
-			state = cs.Mul(state, state) // ^2
+			state = cs.Mul(tmp, tmp)     // ^2
 			state = cs.Mul(state, tmp)   // ^3
 			state = cs.Mul(state, state) // ^6
 			state = cs.Mul(state, tmp)   // ^7
