8b812ec41d2a521c68038c7bc26a8c2c948e5dcf
= <olivier.begassat.cours@gmail.com>
2021-08-12 11:27:44 +0200

    mimc test

--- hash/mimc_value_test.go
+++ hash/mimc_value_test.go
@@ -1,6 +1,7 @@
 package hash
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/consensys/gnark-crypto/ecc/bn254/fr"
@@ -8,14 +9,33 @@ import (
 
 func TestMimcValue(t *testing.T) {
 
+	var h fr.Element
+
+	// hash single element
+	var value1 uint64 = 123
 	var state, block fr.Element
-	state.SetZero()
-	block.SetUint64(123)
+	block.SetUint64(value1)
 	MimcUpdateInplace(&state, block)
+	fmt.Printf("Hash of value1 = %v:\n%v\n", value1, state.String())
 
-	println(state.String())
+	// hash empty slice
+	emptySlice := make([]fr.Element, 0)
+	h = MimcHash(emptySlice)
+	fmt.Printf("Hash of empty slice:\n%v\n", h.String())
 
-	MimcUpdateInplace(&state, block)
+	// hash of [123, 1034]
+	var value2 uint64 = 1034
+	slice := make([]fr.Element, 2)
+	slice[0].SetUint64(value1)
+	slice[1].SetUint64(value2)
+	h = MimcHash(slice)
+	fmt.Printf("Hash of [%v, %v]:\n%v\n", value1, value2, h.String())
 
-	println(state.String())
+	// hash of [123, 1034, 546543257896543245]
+	var value3 uint64 = 546543257896543245
+	var bigFrElement fr.Element
+	bigFrElement.SetUint64(value3)
+	slice = append(slice, bigFrElement)
+	h = MimcHash(slice)
+	fmt.Printf("Hash of [%v, %v, %v]:\n%v\n", value1, value2, value3, h.String())
 }
