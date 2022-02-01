c4c010d9e43d0d43cb77ef5c117b861030756cef
Alexandre Belling <alexandrebelling8@gmail.com>
2022-02-01 23:11:48 +0100

    Uses the evaluator inside the circuit

--- examples/mimc.go
+++ examples/mimc.go
@@ -4,7 +4,49 @@ import (
 	"github.com/consensys/gkr-mimc/circuit"
 	"github.com/consensys/gkr-mimc/circuit/gates"
 	"github.com/consensys/gkr-mimc/hash"
+	"github.com/consensys/gnark-crypto/ecc/bn254/fr"
 )
+
+// Evaluator for all layers except the last
+func evalMimc(i int) circuit.Evaluator {
+	return func(inps []fr.Element) []fr.Element {
+		mid := len(inps) / 2
+		res := make([]fr.Element, len(inps))
+		var tmp fr.Element
+		for h := 0; h < mid; h++ {
+			// perform the cipher operation
+			tmp.Add(&inps[h], &hash.Arks[i])
+			res[h].
+				Square(&tmp).
+				Mul(&res[h], &tmp).
+				Square(&res[h]).
+				Mul(&tmp, &res[h]).
+				Add(&res[h], &inps[h+mid])
+		}
+		// Perform the full copy at once
+		copy(res[mid:], inps[mid:])
+		return res
+	}
+}
+
+func evalLastLayer(i int) circuit.Evaluator {
+	return func(inps []fr.Element) []fr.Element {
+		mid := len(inps) / 2
+		res := make([]fr.Element, mid)
+		var tmp fr.Element
+		for h := 0; h < mid; h++ {
+			// perform the cipher operation
+			tmp.Add(&inps[h], &hash.Arks[i])
+			res[h].
+				Square(&tmp).
+				Mul(&res[h], &tmp).
+				Square(&res[h]).
+				Mul(&tmp, &res[h]).
+				Add(&res[h], &inps[h+mid])
+		}
+		return res
+	}
+}
 
 // CreateMimcCircuit returns the GKR MIMC proving circuit
 func CreateMimcCircuit() circuit.Circuit {
@@ -23,5 +65,13 @@ func CreateMimcCircuit() circuit.Circuit {
 		{L: 1, R: 0, O: 0, Gate: gates.NewCipherGate(hash.Arks[nRounds-1])},
 	}
 
-	return circuit.NewCircuit(wiring)
+	res := circuit.NewCircuit(wiring)
+
+	for i := 0; i < nRounds-1; i++ {
+		res.Layers[i].CustomEvaluator = evalMimc(i)
+	}
+
+	res.Layers[nRounds-1].CustomEvaluator = evalLastLayer(nRounds - 1)
+
+	return res
 }
